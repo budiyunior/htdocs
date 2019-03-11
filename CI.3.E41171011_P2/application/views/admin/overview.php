742344983840ca429358975ee06eefb8129299cb
adigagas <45475971+adigagas@users.noreply.github.com>
2019-03-11 10:30:58 +0700

    Update overview.php

--- CI.3.E41171011_P2/application/views/admin/overview.php
+++ CI.3.E41171011_P2/application/views/admin/overview.php
@@ -12,11 +12,24 @@
   <title>SB Admin 2 - Dashboard</title>
 
   <!-- Custom fonts for this template-->
-  <link href="../assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <!-- <link href="../assets/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet"> -->
 
   <!-- Custom styles for this template-->
-  <link href="../css/sb-admin-2.min.css" rel="stylesheet">
+  <!-- <link href="../css/sb-admin-2.min.css" rel="stylesheet"> -->
+
+  <!-- Bootstrap core CSS-->
+<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>" rel="stylesheet">
+
+<!-- Custom fonts for this template-->
+<link href="<?php echo base_url('assets/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+<!-- Page level plugin CSS-->
+<link href="<?php echo base_url('assets/datatables/dataTables.bootstrap4.css') ?>" rel="stylesheet">
+
+<!-- Custom styles for this template-->
+<link href="<?php echo base_url('css/sb-admin-2.css') ?>" rel="stylesheet">
 
 </head>
 
@@ -656,22 +669,41 @@
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="../assets/jquery/jquery.min.js"></script>
-  <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- <script src="../assets/jquery/jquery.min.js"></script>
+  <script src="../assets/bootstrap/js/bootstrap.bundle.min.js"></script> -->
 
   <!-- Core plugin JavaScript-->
-  <script src="../assets/jquery-easing/jquery.easing.min.js"></script>
+  <!-- <script src="../assets/jquery-easing/jquery.easing.min.js"></script> -->
 
   <!-- Custom scripts for all pages-->
-  <script src="../js/sb-admin-2.min.js"></script>
+  <!-- <script src="../js/sb-admin-2.min.js"></script> -->
 
   <!-- Page level plugins -->
-  <script src="../assets/chart.js/Chart.min.js"></script>
+  <!-- <script src="../assets/chart.js/Chart.min.js"></script> -->
 
   <!-- Page level custom scripts -->
-  <script src="../js/demo/chart-area-demo.js"></script>
-  <script src="../js/demo/chart-pie-demo.js"></script>
+  <!-- <script src="../js/demo/chart-area-demo.js"></script>
+  <script src="../js/demo/chart-pie-demo.js"></script> -->
 
+  <!-- Bootstrap core JavaScript-->
+<script src="<?php echo base_url('assets/jquery/jquery.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
+
+<!-- Core plugin JavaScript-->
+<script src="<?php echo base_url('assets/jquery-easing/jquery.easing.min.js') ?>"></script>
+
+<!-- Page level plugin JavaScript-->
+<script src="<?php echo base_url('assets/chart.js/Chart.min.js') ?>"></script>
+<script src="<?php echo base_url('assets/datatables/jquery.dataTables.js') ?>"></script>
+<script src="<?php echo base_url('assets/datatables/dataTables.bootstrap4.js') ?>"></script>
+
+<!-- Custom scripts for all pages-->
+<script src="<?php echo base_url('js/sb-admin-2.min.js') ?>"></script>
+
+<!-- Demo scripts for this page-->
+<script src="<?php echo base_url('js/demo/datatables-demo.js') ?>"></script>
+<script src="<?php echo base_url('js/demo/chart-area-demo.js') ?>"></script>
+<script src="<?php echo base_url('js/demo/chart-pie-demo.js') ?>"></script>
 </body>
 
 </html>
