2f9061fd9381cbe9e45f0892880d2407e45c680a
bahtiar adi prasetya <bahtiaradi62@gmail.com>
2019-03-25 14:47:29 +0700

    tambah

--- CI.3.E41171467_P2/application/controllers/admin/Login.php
+++ CI.3.E41171467_P2/application/controllers/admin/Login.php
@@ -1,59 +1,15 @@
-!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
+class Overview extends CI_Controller {
+    public function __construct()
+    {
+		parent::__construct();
+	}
 
-<?php $this->load->view("admin/_partials/head.php") ?>
+	public function index()
+	{
+        // load view admin/overview.php
+        $this->load->view("admin/login");
+	}
+}
 
-</head>
-
-<body class="bg-dark">
-
-  <div class="container">
-    <div class="card card-login mx-auto mt-5">
-      <div class="card-header">Login</div>
-      <div class="card-body">
-        <form>
-          <div class="form-group">
-            <div class="form-label-group">
-              <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="required" autofocus="autofocus">
-              <label for="inputEmail">Email address</label>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="form-label-group">
-              <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="required">
-              <label for="inputPassword">Password</label>
-            </div>
-          </div>
-          <div class="form-group">
-            <div class="checkbox">
-              <label>
-                <input type="checkbox" value="remember-me">
-                Remember Password
-              </label>
-            </div>
-          </div>
-          <a class="btn btn-primary btn-block" href="index.html">Login</a>
-        </form>
-        <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="forgot-password.html">Forgot Password?</a>
-        </div>
-      </div>
-    </div>
-  </div>
-  </div>
-		<!-- /.container-fluid -->
-
-		<!-- Sticky Footer -->
-		<?php $this->load->view("admin/_partials/footer.php") ?>
-
-	</div>
-
-  <?php $this->load->view("admin/_partials/scrolltop.php") ?>
-<?php $this->load->view("admin/_partials/modal.php") ?>
-<?php $this->load->view("admin/_partials/js.php") ?>
-</body>
-
-</html>
