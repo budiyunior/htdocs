53ba9e3eb5ccf0033b02fddb659ed6e6c2a5256a
Vinny Sakinah Virgio <vinnysakinah13@gmail.com>
2019-03-25 14:47:31 +0700

    update link

---
+++ CI.3.E41171335_P2/application/controllers/admin/Forgot.php
@@ -0,0 +1,14 @@
+<?php
+
+class Forgot extends CI_Controller {
+    public function __construct()
+    {
+		parent::__construct();
+	}
+
+	public function index()
+	{
+        // load view admin/login.php
+        $this->load->view("admin/forgot");
+	}
+}
--- CI.3.E41171335_P2/application/views/admin/forgot.php
+++ CI.3.E41171335_P2/application/views/admin/forgot.php
@@ -3,20 +3,7 @@
 
 <head>
 
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
-
-  <title>SB Admin - Forgot Password</title>
-
-  <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-
-  <!-- Custom styles for this template-->
-  <link href="css/sb-admin.css" rel="stylesheet">
-
+<?php $this->load->view("admin/_partials/head.php") ?>
 </head>
 
 <body class="bg-dark">
@@ -39,19 +26,15 @@
           <a class="btn btn-primary btn-block" href="login.html">Reset Password</a>
         </form>
         <div class="text-center">
-          <a class="d-block small mt-3" href="register.html">Register an Account</a>
-          <a class="d-block small" href="login.html">Login Page</a>
+          <a class="d-block small mt-3" href="<?= site_url("register") ?>">Register an Account</a>
+          <a class="d-block small" href="<?= site_url("login") ?>">Login Page</a>
         </div>
       </div>
     </div>
   </div>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Core plugin JavaScript-->
-  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <?php $this->load->view("admin/_partials/js.php") ?>
 
 </body>
 
