a67794d3c0c1f8f89ded0837883e047cfd0af7a0
adigagas <45475971+adigagas@users.noreply.github.com>
2019-05-07 00:55:48 +0700

    pulsa.dy web

---
+++ pulsa.dy/application/models/Client_model.php
@@ -0,0 +1,72 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Client_model extends CI_Model
+{
+    private $_table = "pengguna";
+
+    public $id_pengguna;
+    public $nama_pengguna;
+    public $id_otoritas;
+    public $username;
+    public $password;
+
+    public function rules()
+    {
+        return [
+            ['field' => 'nama_pengguna',
+            'label' => 'nama_pengguna',
+            'rules' => 'required'],
+
+            ['field' => 'id_otoritas',
+            'label' => 'id_otoritas',
+            'rules' => 'numeric'],
+            
+            ['field' => 'username',
+            'label' => 'username',
+            'rules' => 'required'],
+
+            ['field' => 'password',
+            'label' => 'password',
+            'rules' => 'required']
+        ];
+    }
+
+    public function getAll()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+    
+    public function getById($id)
+    {
+        return $this->db->get_where($this->_table, ["id_pengguna" => $id])->row();
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        $this->id_pengguna = uniqid();
+        $this->nama_pengguna = $post["nama_pengguna"];
+        $this->id_otoritas = $post["id_otoritas"];
+        $this->username = $post["username"];
+        $this->password = $post["password"];
+        $this->db->insert($this->_table, $this);
+    }
+
+    public function update()
+    {
+        $post = $this->input->post();
+        $this->id_pengguna = $post["id_pengguna"];
+        $this->nama_pengguna = $post["nama_pengguna"];
+        $this->id_otoritas = $post["id_otoritas"];
+        $this->username = $post["username"];
+        $this->password = $post["password"];
+        $this->db->update($this->_table, $this, array('product_id' => $post['id_pengguna']));
+    }
+
+    public function delete($id)
+    {
+        $this->_deleteImage($id);
+        return $this->db->delete($this->_table, array("id_pengguna" => $id));
+    }
+
+}
---
+++ pulsa.dy/application/views/admin/client/list.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<?php $this->load->view("admin/_partials/head.php") ?>
+
+</head>
+
+<body id="page-top">
+
+	<!-- Page Wrapper -->
+	<div id="wrapper">
+
+		<!-- Sidebar -->
+		<?php $this->load->view("admin/_partials/sidebar.php") ?>
+		<!-- End of Sidebar -->
+
+		<!-- Content Wrapper -->
+		<div id="content-wrapper" class="d-flex flex-column">
+
+		<!-- Main Content -->
+		<div id="content">
+
+			<!-- Topbar -->
+			<?php $this->load->view("admin/_partials/navbar.php") ?>
+			<!-- End of Topbar -->
+
+			<!-- Begin Page Content -->
+			<div class="container-fluid">
+
+				<!-- Page Heading -->
+				<h1 class="h3 mb-2 text-gray-800">Product</h1>
+				<p class="mb-4">Product is a third party plugin that is used to generate the demo table below. For more information about DataTables, please visit the <a target="_blank" href="https://datatables.net">official DataTables documentation</a>.</p>
+
+				<!-- DataTales Example -->
+					<div class="card mb-3">
+						<div class="card-header">
+							<a href="<?php echo site_url('admin/client/add') ?>"><i class="fas fa-plus"></i> Add New</a>
+						</div>
+						<div class="card-body">
+
+							<div class="table-responsive">
+								<table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+									<thead>
+										<tr>
+											<th>Nama</th>
+											<th>otoritas</th>
+											<th>username</th>
+											<th>password</th>
+											<th>Action</th>
+										</tr>
+									</thead>
+									<tbody>
+										<?php foreach ($pengguna as $pengguna): ?>
+										<tr>
+											<td width="150">
+												<?php echo $pengguna->nama_pengguna ?>
+											</td>
+											<td>
+												<?php echo $pengguna->id_otoritas ?>
+											</td>
+											<td>
+												<?php echo $pengguna->username ?>
+											</td>
+											<td>
+												<?php echo $pengguna->password ?>
+											</td>
+											<td width="250">
+												<a href="<?php echo site_url('admin/client/edit/'.$pengguna->id_pengguna) ?>"class="btn btn-small"><i class="fas fa-edit"></i> Edit</a>
+												<a onclick="deleteConfirm('<?php echo site_url('admin/client/delete/'.$pengguna->id_pengguna) ?>')"href="#!" class="btn btn-small text-danger"><i class="fas fa-trash"></i> Hapus</a>
+											</td>
+										</tr>
+										<?php endforeach; ?>
+
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<!-- /.container-fluid -->
+
+			</div>
+			<!-- End of Main Content -->
+
+			<!-- Footer -->
+			<?php $this->load->view("admin/_partials/footer.php") ?>
+			<!-- End of Footer -->
+
+		</div>
+		<!-- End of Content Wrapper -->
+
+	</div>
+	<!-- End of Page Wrapper -->
+
+	<!-- Scroll to Top Button-->
+	<?php $this->load->view("admin/_partials/scrolltop.php") ?>
+
+	<!-- Logout Modal-->
+	<?php $this->load->view("admin/_partials/modal.php") ?>
+
+	<!-- JavaScript-->
+	<?php $this->load->view("admin/_partials/js.php") ?>
+	<script>
+		function deleteConfirm(url){
+		$('#btn-delete').attr('href', url);
+		$('#deleteModal').modal();
+		}
+	</script>
+
+</body>
+
+</html>
