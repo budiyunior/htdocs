a692549386ae6cb8c5cb80239241b6e3e8299eec
adigagas <45475971+adigagas@users.noreply.github.com>
2019-05-07 09:23:01 +0700

    Update overview.php

--- pulsa.dy/application/views/admin/overview.php
+++ pulsa.dy/application/views/admin/overview.php
@@ -188,8 +188,178 @@
                     </div>
                     </div>
                 </div>
-            
+
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Telkomsel</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 100.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Telkomsel</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 50.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Telkomsel</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 20.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-danger shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Telkomsel</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 10.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Indosat ooredoo</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 100.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+                
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Indosat ooredoo</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 50.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Indosat ooredoo</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 20.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+                <!-- isi cepat -->
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-bottom-warning shadow h-100 py-2">
+                    <div class="card-body">
+                        <div class="row no-gutters align-items-center">
+                        <div class="col mr-2">
+                            <div class="h4 mb-0 font-weight-bold text-gray-800 mb-1">Indosat ooredoo</div>
+                            <div class="text-xs font-weight-bold text-primary text-uppercase ">Nominal</div>
+                            <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. 10.000</div>
+                        </div>
+                        <div class="col-auto">
+                            <a href="">
+                                <!-- tombol tambah saldo -->
+                                <i class="fas fa-plus fa-2x text-gray-300"></i>
+                            </a>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
             </div>
+
+            
 
         </div>
         <!-- /.container-fluid -->
