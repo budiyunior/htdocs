3bcd9faadf905417c6db263254259bc05d1c0da3
Vinny Sakinah Virgio <vinnysakinah13@gmail.com>
2019-05-27 19:36:47 +0700

    Login sedang Maintance
    Istirahat dulu geng, cari maem, mandi dan sholat.

--- adigagas/application/models/Login_model.php
+++ adigagas/application/models/Login_model.php
@@ -21,15 +21,20 @@ class Login_model extends CI_Model
    
     function auth_admin($username,$password){
         $hash=$this->db->query("SELECT password FROM pengguna WHERE email='$username'");
-        $query=$this->db->get_where('pengguna', array('email'=>$username, 'password'=>password_verify($password,$hash), 'id_akses'=>'adm'));
+        $pass=password_verify($password, $hash);
+        
+         $query=$this->db->query("SELECT * FROM pengguna WHERE email='$username' and password='$pass' and id_akses='adm'");
+        
+        // $query=$this->db->get_where('pengguna', array('email'=>$username, 'password'=>password_verify($password), 'id_akses'=>'adm'));
            
         return $query;
     }
  
    
     function auth_kasir($username,$password){
-        $hash=$this->db->query("SELECT password FROM pengguna WHERE email='$username'");
-        $query=$this->db->get_where('pengguna', array('email'=>$username, 'password'=>password_verify($password,$hash), 'id_akses'=>'cas'));
+        // $hash=$this->db->query("SELECT password FROM pengguna WHERE email='$username' and password='$password'");
+        // $pass=password_verify($hash);
+        $query=$this->db->get_where('pengguna', array('email'=>$username, 'password'=>password_verify($password), 'id_akses'=>'cas'));
       
         return $query;
     }
