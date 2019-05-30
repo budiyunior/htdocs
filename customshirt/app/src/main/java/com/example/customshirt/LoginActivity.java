9beb0f6766639e1cf8effed14ff11ae594df439f
Budi Yunior Valentino <magenzlizrd@gmail.com>
2019-05-30 18:36:49 +0700

    Card grid

--- customshirt/app/src/main/java/com/example/customshirt/LoginActivity.java
+++ customshirt/app/src/main/java/com/example/customshirt/LoginActivity.java
@@ -1,13 +1,30 @@
 package com.example.customshirt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    private Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        login = (Button) findViewById(R.id.btn_login);
+        login.setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+
+        if(view == login){
+            startActivity(new Intent(this,MainActivity.class));
+        }
     }
 }
