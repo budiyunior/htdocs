d17f7f4618f5ec4be19c4f259b37364d6b308cfb
adigagas <45475971+adigagas@users.noreply.github.com>
2019-06-12 11:07:28 +0700

    del

--- rest_ci/application/helpers/json_output_helper.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-defined('BASEPATH') or exit('NO direct script access aloowed');
-
-    function json_output($statusHeader, $response){
-        $ci =& get_instance();
-        $ci->output->set_content_type('application/json')
-    }
