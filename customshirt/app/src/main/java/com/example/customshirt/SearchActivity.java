686db617864086c37287d558fafab2a093e42ee6
bahtiar adi prasetya <bahtiaradi62@gmail.com>
2019-06-19 23:39:27 +0700

    search activity

--- customshirt/app/src/main/java/com/example/customshirt/SearchActivity.java
+++ customshirt/app/src/main/java/com/example/customshirt/SearchActivity.java
@@ -1,13 +1,103 @@
 package com.example.customshirt;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import com.example.customshirt.Adapter.ItemAdapter;
+import com.example.customshirt.Model.GetItem;
+import com.example.customshirt.Model.Item;
+import com.example.customshirt.Rest.ApiClient;
+import com.example.customshirt.Rest.ApiInterface;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SearchActivity extends AppCompatActivity {
+
+    ApiInterface mApiInterface;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public static MainActivity ma;
+
+    private List<Item> itemList;
+    ProgressBar progressBar;
+    TextView search;
+    String[] item;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        progressBar = findViewById(R.id.prograss);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        mLayoutManager = new GridLayoutManager(this,2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        refresh();
+
     }
+
+    public void refresh() {
+        Call<GetItem> ItemCall = mApiInterface.getItem();
+        ItemCall.enqueue(new Callback<GetItem>() {
+            @Override
+            public void onResponse(Call<GetItem> call, Response<GetItem>
+                    response) {
+                List<Item> itemList = response.body().getListDataItem();
+                Log.d("Retrofit Get", "Jumlah data Item: " +
+                        String.valueOf(itemList.size()));
+                mAdapter = new ItemAdapter(itemList);
+                mRecyclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<GetItem> call, Throwable t) {
+                Log.e("Retrofit Get", t.toString());
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                refresh();
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                refresh();
+                return false;
+            }
+        });
+        return true;
+    }
+
 }
