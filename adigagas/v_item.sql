9502f903d311c4837db1e9153563097472175c4e
helmihppn <47851567+helmihppn@users.noreply.github.com>
2019-07-11 01:17:34 +0700

    GANTIEN VIEW ITEM

--- adigagas/v_item.sql
+++ adigagas/v_item.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2019 at 02:04 PM
+-- Generation Time: Jul 10, 2019 at 08:16 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.0.32
 
