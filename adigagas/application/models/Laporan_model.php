fda8c59372788d6009ba43cad4d64e8ac923d29d
Vinny Sakinah Virgio <vinnysakinah13@gmail.com>
2019-07-10 21:38:50 +0700

    laporan

---
+++ adigagas/application/models/Laporan_model.php
@@ -0,0 +1,109 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+class Laporan_model extends CI_Model
+{
+    private $_table = "pengguna";
+
+    public $id_pengguna;
+    public $nama_pengguna;
+    public $tanggal_lahir;
+    public $email;
+    public $password;
+    public $nomor_telp;
+    public $foto = "default.jpg";
+
+    public function rules()
+    {
+        return [
+
+            [
+                'field' => 'nama_pengguna',
+                'label' => 'nama_pengguna',
+                'rules' => 'required'
+            ],
+
+        ];
+    }
+
+    public function getAll()
+    {
+        return $this->db->get($this->_table)->result();
+    }
+
+    public function getById($id)
+    {
+        return $this->db->get_where($this->_table, ["id_pengguna" => $id])->row();
+    }
+
+    public function getUserId()
+    {
+        $query = $this->db->query("SELECT * FROM v_pengguna WHERE id_akses ='cus'");
+        return $query->result();
+    }
+
+    public function save()
+    {
+        $post = $this->input->post();
+        $this->id_pengguna = "cus" . uniqid(15);
+        $this->nama_pengguna = $post["nama_pengguna"];
+        $this->tanggal_lahir = $post["tanggal_lahir"];
+        $this->id_akses = "cus";
+        $this->email = $post["email"];
+        $this->password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+        $this->nomor_telp = $post["nomor_telp"];
+        $this->foto = $this->_uploadImage();
+        $this->db->insert($this->_table, $this);
+    }
+
+    public function update()
+    {
+        $post = $this->input->post();
+        $this->id_pengguna = $post["id_pengguna"];
+        $this->nama_pengguna = $post["nama_pengguna"];
+        $this->tanggal_lahir = $post["tanggal_lahir"];
+        $this->id_akses = $post["id_akses"];
+        $this->email = $post["email"];
+        $this->password = $post["password"];
+        $this->nomor_telp = $post["nomor_telp"];
+        if (!empty($_FILES["foto"]["name"])) {
+            $this->foto = $this->_uploadImage();
+        } else {
+            $this->foto = $post["old_image"];
+        }
+        $this->db->update($this->_table, $this, array('id_pengguna' => $post['id_pengguna']));
+        $this->db->update($this->_table, $this, array('id_pengguna' => $post['id_pengguna']));
+    }
+
+    public function delete($id_pengguna)
+    {
+        return $this->db->delete($this->_table, array("id_pengguna" => $id_pengguna));
+    }
+
+    private function _uploadImage()
+    {
+        $config['upload_path']          = './upload/profil/';
+        $config['allowed_types']        = 'gif|jpg|png';
+        $config['file_name']            = $this->id_pengguna;
+        $config['overwrite']            = true;
+        $config['max_size']             = 1024; // 1MB
+        // $config['max_width']            = 1024;
+        // $config['max_height']           = 768;
+
+        $this->load->library('upload', $config);
+
+        if ($this->upload->do_upload('foto')) {
+            return $this->upload->data("file_name");
+        }
+
+        print_r($this->upload->display_errors());
+    }
+
+    private function _deleteImage($id_pengguna)
+    {
+        $pelanggan = $this->getById($id_pengguna);
+        if ($pelanggan->foto != "01.jpg") {
+            $filename = explode(".", $pelanggan->foto)[0];
+            return array_map('unlink', glob(FCPATH . "upload/profil/$filename.*"));
+        }
+    }
+}
--- adigagas/application/views/admin/_partials/sidebar.php
+++ adigagas/application/views/admin/_partials/sidebar.php
@@ -1,78 +1,78 @@
 <!-- Sidebar -->
-    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-      <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo site_url('admin') ?>">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-tshirt"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">Custom Shirt</div>
-      </a>
+  <!-- Sidebar - Brand -->
+  <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo site_url('admin') ?>">
+    <div class="sidebar-brand-icon rotate-n-15">
+      <i class="fas fa-tshirt"></i>
+    </div>
+    <div class="sidebar-brand-text mx-3">Custom Shirt</div>
+  </a>
 
-      <!-- Divider -->
-      <hr class="sidebar-divider my-0">
+  <!-- Divider -->
+  <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item <?php echo $this->uri->segment(2) == 'overview' ? 'active': '' ?>">
-        <a class="nav-link" href="#">
-          <i class="fas fa-fw fa-wallet"></i>
-          <span>Transaksi</span></a>
-      </li>
+  <!-- Nav Item - Dashboard -->
+  <li class="nav-item <?php echo $this->uri->segment(2) == 'overview' ? 'active' : '' ?>">
+    <a class="nav-link" href="#">
+      <i class="fas fa-fw fa-wallet"></i>
+      <span>Transaksi</span></a>
+  </li>
 
-      <!-- Nav Item - Pages Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-          <i class="fas fa-fw fa-tshirt"></i>
-          <span>Item</span>
-        </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Item:</h6>
-            <a class="collapse-item" href="<?php echo site_url('admin/item/index') ?>">Item</a>
-            <a class="collapse-item" href="<?php echo site_url('admin/jenisitem/index') ?>">Jenis Item</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Pengguna" aria-expanded="true" aria-controls="Pengguna">
-          <i class="fas fa-fw fa-user"></i>
-          <span>Pengguna</span>
-        </a>
-        <div id="Pengguna" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Pengguna:</h6>
-            <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-color' ? 'active': '' ?>" href="<?php echo site_url('admin/pegawai/index') ?>">Pegawai</a>
-            <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-border' ? 'active': '' ?>" href="<?php echo site_url('admin/pelanggan/index') ?>">Pelanggan</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Nav Item - Utilities Collapse Menu -->
-      <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Laporan" aria-expanded="true" aria-controls="Laporan">
-          <i class="fas fa-fw fa-print"></i>
-          <span>Laporan</span>
-        </a>
-        <div id="Laporan" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-          <div class="bg-white py-2 collapse-inner rounded">
-            <h6 class="collapse-header">Laporan Penjualan:</h6>
-            <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-color' ? 'active': '' ?>" href="#">ALL</a>
-            <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-border' ? 'active': '' ?>" href="#">Tahunan</a>
-            <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-color' ? 'active': '' ?>" href="#">Bulanan</a>
-            <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-border' ? 'active': '' ?>" href="#">Mingguan</a>
-          </div>
-        </div>
-      </li>
-
-      <!-- Divider -->
-      <hr class="sidebar-divider d-none d-md-block">
-
-      <!-- Sidebar Toggler (Sidebar) -->
-      <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  <!-- Nav Item - Pages Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+      <i class="fas fa-fw fa-tshirt"></i>
+      <span>Item</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Item:</h6>
+        <a class="collapse-item" href="<?php echo site_url('admin/item/index') ?>">Item</a>
+        <a class="collapse-item" href="<?php echo site_url('admin/jenisitem/index') ?>">Jenis Item</a>
       </div>
+    </div>
+  </li>
 
-    </ul>
-    <!-- End of Sidebar -->
+  <!-- Nav Item - Utilities Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Pengguna" aria-expanded="true" aria-controls="Pengguna">
+      <i class="fas fa-fw fa-user"></i>
+      <span>Pengguna</span>
+    </a>
+    <div id="Pengguna" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Pengguna:</h6>
+        <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-color' ? 'active' : '' ?>" href="<?php echo site_url('admin/pegawai/index') ?>">Pegawai</a>
+        <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-border' ? 'active' : '' ?>" href="<?php echo site_url('admin/pelanggan/index') ?>">Pelanggan</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Nav Item - Utilities Collapse Menu -->
+  <li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#Laporan" aria-expanded="true" aria-controls="Laporan">
+      <i class="fas fa-fw fa-print"></i>
+      <span>Laporan</span>
+    </a>
+    <div id="Laporan" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
+      <div class="bg-white py-2 collapse-inner rounded">
+        <h6 class="collapse-header">Laporan Penjualan:</h6>
+        <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-color' ? 'active' : '' ?>" href="<?php echo site_url('admin/laporan/index') ?>">ALL</a>
+        <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-border' ? 'active' : '' ?>" href="#">Tahunan</a>
+        <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-color' ? 'active' : '' ?>" href="#">Bulanan</a>
+        <a class="collapse-item <?php echo $this->uri->segment(2) == 'utilities-border' ? 'active' : '' ?>" href="#">Mingguan</a>
+      </div>
+    </div>
+  </li>
+
+  <!-- Divider -->
+  <hr class="sidebar-divider d-none d-md-block">
+
+  <!-- Sidebar Toggler (Sidebar) -->
+  <div class="text-center d-none d-md-inline">
+    <button class="rounded-circle border-0" id="sidebarToggle"></button>
+  </div>
+
+</ul>
+<!-- End of Sidebar -->
